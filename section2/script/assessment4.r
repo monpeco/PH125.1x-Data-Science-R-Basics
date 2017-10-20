beda795abd829ec04e9b1ca43dff2bc7b7e082b9
monpeco <monpeco@gmail.com>
2017-10-20 16:11:00 +0000

    Add assessment4 (data frames, ranks and orders)

--- section2/script/assessment4.r
+++ section2/script/assessment4.r
@@ -139,3 +139,32 @@ ranks <- rank(murders$population)
 
 # Create a data frame my_df with the state name and its rank
 my_df <- data.frame(name=states, rank=ranks)
+
+
+### Data Frames, Ranks and Orders
+
+# This exercise is somewhat more challenging. We are going to repeat the previous exercise 
+# but this time order my_df so that the states are ordered from least populous to most.
+
+# Instructions
+# Create variables states and ranks to store the state names and ranks by population size respectively.
+#
+# Create an object ind that stores the indexes needed to order the population values, using the order 
+# command. For example we could define o <- order(murders$population)
+#
+# Create a data frame with both variables following the correct order. Use the bracket 
+# operator [ to re-order each column in the data frame. For example, states[o] orders the 
+# abbreviations based by population size.
+
+
+# Define a variable states to be the state names from the murders data frame
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <- rank(murders$population)
+
+# Define a variable ind to store the indexes needed to order the population values
+ind <- order(murders$population)
+
+# Create a data frame my_df with the state name and its rank and ordered from least populous to most 
+my_df <- data.frame(name=states[ind], rank=ranks[ind])
