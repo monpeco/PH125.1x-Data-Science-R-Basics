fc6fc66ef4455d3ae95403651baab417695a9736
monpeco <monpeco@gmail.com>
2017-11-02 00:28:52 +0000

    Add assessment 9 (part 2, function definitions)

--- section4/script/assessment9.r
+++ section4/script/assessment9.r
@@ -43,3 +43,61 @@ ifelse(x == -999, NA, x)
 new_names <- ifelse(nchar(murders$state)>8, murders$abb, murders$state)
 
 
+
+# Defining functions
+
+# You will encounter situations in which the function you need does not already exist. R permits you to write your own. Let's practice one such situation, in which you first need to define the function to be used. The functions you define can have multiple arguments as well as default values.
+
+# To define functions we use function. For example the following function adds 1 to the number it receives as an argument:
+
+my_func <- function(x){
+    y <- x + 1
+    y
+}
+
+# The last value in the function, in this case that stored in y, gets returned.
+
+# If you run the code above R does not show anything. This means you defined the function. You can test it out like this:
+
+my_func(5)
+
+# Instructions
+# We will define a function sum_n for this exercise.
+
+# Create a function sum_n that for any given value, say n, creates the vector 1:n, and then computes the sum of the integers from 1 to n.
+# Use the function you just defined to determine the sum of integers from 1 to 5,000.
+
+# Create function called `sum_n`
+sum_n <- function(n){
+  x <- 1:n
+  sum(x)
+}
+# Use the function to determine the sum of integers from 1 to 5000
+sum_n(5000)
+
+
+
+# Defining functions continued...
+
+# We will make another function for this exercise. We will define a function altman_plot that takes two arguments x and y and plots the difference y-x in the y-axis against the sum x+y in the x-axis.
+
+# You can define functions with as many variables as you want. For example, here we need at least two, x and y. The following function plots log transformed values:
+
+log_plot <- function(x, y){
+    plot(log10(x), log10(y))
+}
+
+# This function does not return anything. It just makes a plot.
+
+# Instructions
+# We will make another function for this exercise.
+
+# Create a function altman_plot that takes two arguments x and y and plots y-x (on the y-axis) against x+y (on the x-axis).
+
+# Create `altman_plot` 
+altman_plot <- function(x,y){
+  plot(x+y, y-x)
+}
+
+
+
